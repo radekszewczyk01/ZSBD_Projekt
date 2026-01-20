7b21ee2a165b4f4c4bd983fe9505820ebb8c0333
Radoslaw Szewczyk <radekszewczyk01@gmail.com>
2026-01-20 10:17:17 +0100

    small fixes

--- login_nowy_admin.sh
+++ login_nowy_admin.sh
@@ -3,4 +3,4 @@
 # OSTRZEŻENIE: Hasło w tym pliku jest widoczne jako zwykły tekst!
 # Używaj tylko na komputerze, do którego NIKT inny nie ma dostępu.
 
-mysql -u nowy_admin -pSilneHaslo123! --init-command="USE rpc;"
+mysql -u rpc_admin -pSilnehaslo123. --init-command="USE rpc;"
